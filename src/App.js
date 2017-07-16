36e401b59f8b8065a8e4d4d597865e49c242801d
Nouman Memon <noumanm@innmenon.local>
2017-07-16 19:28:42 +0530

    1) Added react-bootstrap 2) simple form to accept First Name, Last Name and Type 3) connected to Users service to create new user into Postgres DB

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
 import './App.css';
 
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {fname: ''};
-    this.state = {lname: ''};
-    this.state = {type: 'Buyer'};
+    this.state = {
+      fname: '',
+      lname: '',
+      type: 'Buyer',
+    };
 
     this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
     this.handleLastNameChange = this.handleLastNameChange.bind(this);
@@ -26,13 +29,46 @@ class App extends Component {
     this.setState({type: event.target.value});
   }
 
-  handleSubmit(event) {
-    alert('First Name: ' + this.state.fname + ' \n Last Name :' + this.state.lname + ' \n Type:' + this.state.type);
+  createUser () {
+    fetch('http://localhost:8080/createUser', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        fname: this.state.fname,
+        lname: this.state.lname,
+        type: this.state.type,
+      })
+    }).then(function(response) {
+        return response.json();
+      }).then(function(text) { 
+        // <!DOCTYPE ....
+        alert('User Created: SUCCESS \n First Name: '+text.fname + ' \n Last Name:' +text.lname+'\n Type:'+text.type);
+        console.log(text); 
+      });
+  }
+
+  defaultStateAfterCreate () {
+    this.setState({fname: ''});
+    this.setState({lname: ''});
+    this.setState({type: 'Buyer'});
+  }
+
+  handleSubmit(event) {  
+    this.createUser ();
     event.preventDefault();
+    this.defaultStateAfterCreate ();
   }
 
   render() {
+    const { fname, lname, type } = this.state;
+    const isEnabled =
+         fname.length > 0 &&
+         lname.length > 0;
     return (
+      
+
       <div> {
 
       }
@@ -57,7 +93,7 @@ class App extends Component {
         </div>
 
         <div className="form-group">
-          <button type="button" className="btn btn-primary" onClick={this.handleSubmit}>Create User</button> {/* Some form attributes use an expression to set true or false: they include disabled, required, checked and readOnly */}
+          <button type="button" className="btn btn-primary" disabled={!isEnabled} onClick={this.handleSubmit}>Create User</button> {/* Some form attributes use an expression to set true or false: they include disabled, required, checked and readOnly */}
         </div>
       </div>
 
